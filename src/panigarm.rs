3daad1e8dbd35266152a2e199e3232d805cdcc1e
hisanori <hisary@gmail.com>
2025-01-29 16:23:39 +0900

    update panigarm

--- src/panigarm.rs
+++ src/panigarm.rs
@@ -5,7 +5,7 @@ use chrono_tz::Asia::Tokyo;
 pub struct Boss {
    pub name: &'static str
 }
-const ALMANA: Boss = Boss { name: "源世鳥アルマナ" };
+const BIRD: Boss = Boss { name: "源世鳥アルマナ" };
 const DRAGON: Boss = Boss { name: "じげんりゅう" };
 const CATCHER: Boss = Boss { name: "パニガキャッチャー" };
 const FRUITS: Boss = Boss { name: "源世果フルポティ" };
@@ -14,17 +14,17 @@ const PRISON: Boss = Boss { name: "源世妃フォルダイナ" };
 const STAR: Boss = Boss { name: "魔妖星プルタヌス" };
 const IRON: Boss = Boss { name: "鉄巨兵ダイダルモス" };
 
-// 2023-05-21 6:00を起点としたスケジュール
-// v6.5は6/14から開始、新ボスの初回登場は6/18
+// v7.3, 2025-01-29 6:00を起点とするスケジュール。三日周期でボスが変わる。
+// 順番は変わらない想定
 const SEQUENCE: [&Boss; 8] = [
-    &CORVUS,
-    &ALMANA,
+    &BIRD,
     &DRAGON,
     &PRISON,
     &IRON,
     &CATCHER,
     &FRUITS,
     &STAR,
+    &CORVUS,
 ];
 
 pub fn get_current_boss() -> &'static Boss {
@@ -35,14 +35,14 @@ pub fn get_boss<Tz: TimeZone>(dt: DateTime<Tz>) -> &'static Boss {
     let base_point = get_base_point();
     let calc_point = dt.naive_utc();
     let duration = calc_point - base_point;
-    let pass = duration.num_weeks() as usize;
+    let pass = (duration.num_days() / 3) as usize;
     let index = pass % SEQUENCE.len();
     SEQUENCE[index]
 }
 
 fn get_base_point() -> NaiveDateTime {
-    // 2023-05-21 06:00:00 JST
-    Tokyo.with_ymd_and_hms(2023, 5, 21, 6, 0, 0).unwrap().naive_utc()
+    // 2025-01-29 06:00:00 JST (v7.3 公開日)
+    Tokyo.with_ymd_and_hms(2025, 1, 29, 6, 0, 0).unwrap().naive_utc()
 }
 
 
@@ -52,32 +52,30 @@ mod tests {
 
     #[test]
     fn test_get_boss() {
-        // 切り替わりの境界テスト
         let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
-        let dt = tz.with_ymd_and_hms(2023, 7, 9, 5, 59, 59).single().unwrap();
+        let dt = tz.with_ymd_and_hms(2025, 1, 29, 13, 0, 0).single().unwrap();
         let boss = super::get_boss(dt);
-        assert_eq!(boss.name, "源世果フルポティ");
+        assert_eq!(boss.name, "源世鳥アルマナ");
 
         let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
-        let dt = tz.with_ymd_and_hms(2023, 7, 9, 6, 0, 0).single().unwrap();
+        let dt = tz.with_ymd_and_hms(2025, 2, 1, 5, 59, 59).single().unwrap();
         let boss = super::get_boss(dt);
-        assert_eq!(boss.name, "魔妖星プルタヌス");
+        assert_eq!(boss.name, "源世鳥アルマナ");
 
-        // 繰り返しテスト
         let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
-        let dt = tz.with_ymd_and_hms(2023, 5, 21, 6, 0, 0).single().unwrap();
+        let dt = tz.with_ymd_and_hms(2025, 2, 1, 6, 0, 0).single().unwrap();
+        let boss = super::get_boss(dt);
+        assert_eq!(boss.name, "じげんりゅう");
+
+        // 繰り返しテスト, 24日後に同じボスが出現
+        let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
+        let dt = tz.with_ymd_and_hms(2025, 2, 19, 6, 0, 0).single().unwrap();
         let boss = super::get_boss(dt);
         assert_eq!(boss.name, "堕天使エルギオス");
 
         let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
-        let dt = tz.with_ymd_and_hms(2023, 7, 16, 12, 0, 0).single().unwrap(); // 7/23週に再度発生
+        let dt = tz.with_ymd_and_hms(2023, 3, 25, 6, 0,0).single().unwrap();
         let boss = super::get_boss(dt);
         assert_eq!(boss.name, "堕天使エルギオス");
-
-        // 新ボス初回
-        let tz = chrono::FixedOffset::east_opt(9 * 3600).unwrap();
-        let dt = tz.with_ymd_and_hms(2023, 6, 18, 6, 0, 0).single().unwrap();
-        let boss = super::get_boss(dt);
-        assert_eq!(boss.name, "鉄巨兵ダイダルモス");
     }
 }
